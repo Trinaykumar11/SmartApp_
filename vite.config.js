e3e1387f28e17750aa1b73e1607a6d552916b3bf
Balapuram TrinayKumar <trinay.balapuram@ctrls.in>
2026-01-08 16:01:28 +0530

    ViteConfigUpdated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/smartapp/',   
+  base: '/SmartApp_/'   // MUST match repo name exactly
 })
